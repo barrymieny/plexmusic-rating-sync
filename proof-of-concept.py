6f0c88f5556e94295b5284fbf2c30ed7efa669a8
= <=>
2022-12-08 21:54:20 +0000

    cleanup

--- proof-of-concept.py
+++ proof-of-concept.py
@@ -46,39 +46,37 @@ print('connected')
 for album in plex.library.section('Music').albums():
     print('#################Checking out album: ' + str(album))
     for track in album.tracks():
-        # Iterates across all tracks
         
-        if isinstance(track.userRating, float): 
-            # Checks to see if the Plex userRating exists, var type will be float if track is rated, it will be nonetype if not.
-            
-            print(track.title + ' is already rated in Plex, checking if export is needed...')
-            file = loadFile(track.locations[0])
-            #ratings on both? we prefer plex for now
-            # this case is where Plex has rating but the iD3 tag does so the next steps is to write it on the file
-            file.tag.popularities.set(b'MusicBee',convertRatingsToMusicBee(float(track.userRating)),0)
-            file.tag.save()
-            ratingFrame = getRatingValueFromFile(file)
-            print('... ID3 tag rating value of ' + str(ratingFrame.rating) + ' saved from Plex (' + str(convertRatingsToPlex(ratingFrame.rating)) + ')')
-            justsynced += 1 
-        else:
-            # This case is where Plex has no rating so it tries to fetch a value from the id3 tag
-            
-            print(track.title + ' has no rating in Plex, checking file id3 tag')
-            try: 
+        # Iterates across all tracks
+        try:
+            if isinstance(track.userRating, float): 
+                # Checks to see if the Plex userRating exists, var type will be float if track is rated, it will be nonetype if not.
+                
+                print(track.title + ' is already rated in Plex, exporting..')
+                file = loadFile(track.locations[0])
+                # Ratings on both? We prefer plex to dictate for now, so we always write it on the file if present
+                file.tag.popularities.set(b'MusicBee',convertRatingsToMusicBee(float(track.userRating)),0)
+                file.tag.save()
+                ratingFrame = getRatingValueFromFile(file)
+                print('ID3 tag rating value of ' + str(ratingFrame.rating) + ' saved from Plex (' + str(convertRatingsToPlex(ratingFrame.rating)) + ')' )
+                justsynced += 1 
+            else:
+                # This case is where Plex has no rating so it tries to fetch a value from the id3 tag
+                
+                print(track.title + ' has no rating in Plex, checking file id3 tag')
                 file = loadFile(track.locations[0])
                 ratingFrame = getRatingValueFromFile(file)
                 if ratingFrame is not None:
                     # this case is where Plex has no rating but the iD3 tag does so the next steps import the rating into Plex.
                     track.rate(convertRatingsToPlex(ratingFrame.rating))
-                    print('... ID3 tag rating value of ' + ratingFrame.rating + ' found and saved to Plex userRating field (' + convertRatingsToPlex(ratingFrame.rating) + ')')
+                    print('ID3 tag rating value of ' + str(ratingFrame.rating) + ' found and saved to Plex userRating field (' + str(convertRatingsToPlex(ratingFrame.rating)) + ')')
                     justsynced += 1 
                 else:
-                    print('... failed to read id3 tag rating value for '+ track.title + '. Missing?')
+                    print('No tag present on either side, continuing.')
                     notag += 1
-            except:        
-                # This case is where eyeD3 raises an error due to issues with loading the tag
-                print('... Error loading id3 tags')
-                error += 1
+        except:
+            print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Some odd problem with the file.. continuing.')
+            error += 1
 
 # Stats Output                          
 print(str(insync) + ' files alread in sync')
